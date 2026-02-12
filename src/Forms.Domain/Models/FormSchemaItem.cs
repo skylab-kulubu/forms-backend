5d57242888245b9af3c2ca3be3eb253657220cc8
Fatih Naz <86602083+fatiihnaz@users.noreply.github.com>
2026-02-13 00:45:40 +0300

    Added condition to FormSchemaItem

--- src/Forms.Domain/Models/FormSchemaItem.cs
+++ src/Forms.Domain/Models/FormSchemaItem.cs
@@ -5,4 +5,5 @@ public class FormSchemaItem
     public string Id {get; set;} = string.Empty;
     public string Type {get; set;} = string.Empty;
     public Dictionary<string, object?> Props { get; set; } = new();
+    public Dictionary<string, object?> Condition {get; set;} = new();
 }
