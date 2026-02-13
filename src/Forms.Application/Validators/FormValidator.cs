273176312e693675815ce006e14c8b6260f3e379
Fatih Naz <86602083+fatiihnaz@users.noreply.github.com>
2026-02-13 12:11:02 +0300

    Small update on FormValdiator

--- src/Forms.Application/Validators/FormValidator.cs
+++ src/Forms.Application/Validators/FormValidator.cs
@@ -5,14 +5,19 @@ namespace Forms.Application.Validators;
 
 public static class FormValidator
 {
-    public static ServiceResult<bool> ValidateUpsert(bool allowAnonymous, bool allowMultiple, List<FormSchemaItem>? schema)
+    public static ServiceResult<bool> ValidateUpsert(bool allowAnonymous, bool allowMultiple, List<FormSchemaItem> schema)
     {
         if (allowAnonymous && !allowMultiple)
         {
             return new ServiceResult<bool>(FormAccessStatus.NotAcceptable, Message: "Anonim formlarda çoklu yanıt özelliği açık olmalıdır.");
         }
 
-        // File upload anonimlik kontrolü gelicek
+        bool hasFileField = schema.Any(x => x.Type.Equals("file"));
+
+        if (hasFileField)
+        {
+            return new ServiceResult<bool>(FormAccessStatus.NotAcceptable, Message: "Şimdilik dosya yükleme özelliği desteklenmiyor.");
+        }
 
         return new ServiceResult<bool>(FormAccessStatus.Available, Data: true);
     }
